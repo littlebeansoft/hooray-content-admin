c066a4eab03c7322c99367e5192e07ebcbefa1d9
kittipongkp <kittipong.na@littlebeansoft.com>
2022-08-30 12:22:09 +0700

    Update index.tsx

--- pages/org/[orgToken]/lead/index.tsx
+++ pages/org/[orgToken]/lead/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainLayout from 'layouts/MainLayout/MainLayout'
 import TitleComponent from 'components/TitleComponent'
+import LeadDataTableCard from 'components/Lead/LeadDataTableCard'
 import withAuth from 'middlewares/withAuth'
 
 const AllLead: React.FC = () => {
   return (
     <MainLayout breadcrumb={['Home', '']}>
       <TitleComponent title="Lead" />
+      <LeadDataTableCard />
     </MainLayout>
   )
 }
