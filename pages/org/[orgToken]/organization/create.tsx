a58dd53f56856522e39cbe5bbd327f7165654b0b
kittipongkp <kittipong.na@littlebeansoft.com>
2022-08-31 15:18:12 +0700

    BB-990 Add Page Create Organization

---
+++ pages/org/[orgToken]/organization/create.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import MainLayout from 'layouts/MainLayout/MainLayout'
+import TitleComponent from 'components/TitleComponent'
+import withAuth from 'middlewares/withAuth'
+import { useRouter } from 'next/router'
+import OrganizationCreateCard from 'components/Organization/OrganizationCreate/OrganizationCreateCard'
+
+const LeadCreatePack: React.FC = () => {
+    const router = useRouter()
+
+    return (
+        <MainLayout breadcrumb={['Home', 'Add New Organization']}>
+            <TitleComponent
+                title="Add New Organization"
+                onBack={() =>
+                    router.push({
+                        pathname: `/org/[orgToken]/organization`,
+                        query: {
+                            ...router.query,
+                        },
+                    })
+                }
+            />
+            <OrganizationCreateCard />
+        </MainLayout>
+    )
+}
+
+export default withAuth(LeadCreatePack)
