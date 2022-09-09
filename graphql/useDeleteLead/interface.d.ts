9a9da1f0674e41b4cb891cd3a80aa8c174f206a8
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-09 20:54:36 +0700

    BB-1012 add api delete lead

---
+++ graphql/useDeleteLead/interface.d.ts
@@ -0,0 +1,18 @@
+import { APIPayloadResponse } from 'graphql/graphQL-service-hook'
+
+
+
+interface DeleteLeadPayload {
+    _id: string
+}
+
+export interface DeleteLeadInput {
+    leadId: string
+  }
+  
+  export interface DeleteLeadData {
+    createLead: APIPayloadResponse<DeleteLeadPayload>
+  }
+
+
+
