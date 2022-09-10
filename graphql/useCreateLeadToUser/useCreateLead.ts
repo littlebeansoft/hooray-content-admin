6a0204db9365cca5f7f3db18ee823d4fa8205023
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-10 20:18:35 +0700

    BB-1013 add useCreateLeadToUser

---
+++ graphql/useCreateLeadToUser/useCreateLead.ts
@@ -0,0 +1,18 @@
+import { useMutation } from '@apollo/client'
+import { message } from 'antd'
+
+import QUERY_DOCUMENT from './createLeadToUser'
+
+import type { GraphQLServiceMutationHook } from '../graphQL-service-hook'
+import type { LeadInput, CreateLead } from './interface'
+
+const useCreateLead: GraphQLServiceMutationHook<CreateLead, LeadInput> = (options) => {
+    return useMutation(QUERY_DOCUMENT, {
+        onError(err) {
+            message.error(err)
+        },
+        ...options,
+    })
+}
+
+export default useCreateLead
