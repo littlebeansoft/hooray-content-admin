565ac30c26ea3363e95886436732c05adec3f66c
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-10 20:19:52 +0700

    BB-1013 update createLead card

--- components/Lead/LeadCreate/LeadCreateCard.tsx
+++ components/Lead/LeadCreate/LeadCreateCard.tsx
@@ -1,20 +1,24 @@
 import { Card, Form, message } from 'antd'
 import FullWidthSpace from 'components/FullWidthSpace'
 import useCreateLead from 'graphql/useCreateLead'
-import { CreateLeadInput } from 'graphql/useCreateLead/interface'
+import useCreateLeadToUser from 'graphql/useCreateLeadToUser'
+import useGetLeadData from 'graphql/useGetLeadData'
+import GET_LEAD from 'graphql/useGetLeadData/getLeadData'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import LeadCreateForm from './LeadCreateForm'
-import QUERY_LEAD from 'graphql/useGetLeadData/getLeadData'
+
+
 
 const LeadCreateCard: React.FC = () => {
     const router = useRouter()
+
 
     const [form] = Form.useForm()
 
 
 
-    const [createLead, createLeadResp] = useCreateLead({
+    const [createLead] = useCreateLead({
         onCompleted() {
             message.success('Created Lead was Successfully')
             router.push({
@@ -26,40 +30,56 @@ const LeadCreateCard: React.FC = () => {
         },
     })
 
+    const [createLeadToUser] = useCreateLeadToUser({
+        onCompleted() {
+            message.success('Created User was Successfully')
+            router.push({
+                pathname: `/org/[orgToken]/lead`,
+                query: {
+                    ...router.query,
+                },
+            })
+        },
+    })
+
+
+
     const onFinish = (values: any) => {
         //console.log("Value: -->" + JSON.stringify(values));
-
-        createLead({
-            context: { clientType: 'CUSTOMER' },
-            variables: {
-                input: {
-                    ...values,
-                    image: values?.image[0],
-                    phone: [
-                        { value: values.phone }
-                    ],
-                    email: [
-                        { value: values.email }
-                    ]
-                }
-            },
-            refetchQueries: [
-                {
-                    query: QUERY_LEAD,
-                    context: { clientType: 'CUSTOMER' },
-                    fetchPolicy: 'network-only',
-                    variables: {
-                        input: {
-                            pagination: {
-                                limit: 10,
-                                page: 1,
-                            }
-                        }
+        if (values.leadType === 'FACTORY' || values.leadType === 'AGENT') {
+            createLead({
+                context: { clientType: 'CUSTOMER' },
+                variables: {
+                    input: {
+                        ...values,
+                        image: values?.image[0],
+                        phone: [
+                            { value: values.phone }
+                        ],
+                        email: [
+                            { value: values.email }
+                        ]
                     }
+                },
+            })
+        } else {
+            createLeadToUser({
+                context: { clientType: 'CUSTOMER' },
+                variables: {
+                    input: {
+                        ...values,
+                        image: values?.image[0],
+                        phone: [
+                            { value: values.phone }
+                        ],
+                        email: [
+                            { value: values.email }
+                        ]
+                    }
+                },
+            })
+        }
 
-                }
-            ]
-        })
     }
 
     return (
