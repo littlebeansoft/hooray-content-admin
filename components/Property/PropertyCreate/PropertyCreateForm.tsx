e0aa737f67171a19b2efcdcdcf699b346cfdabf0
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-12 18:22:26 +0700

    update

--- components/Property/PropertyCreate/PropertyCreateForm.tsx
+++ components/Property/PropertyCreate/PropertyCreateForm.tsx
@@ -1,121 +1,210 @@
-import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons'
-import { Button, Col, DatePicker, Form, Input, Row, Select, Spin, Typography } from 'antd'
+import { PlusCircleOutlined, DeleteOutlined, PlusOutlined, DragOutlined } from '@ant-design/icons'
+import { Button, Col, Form, Input, List, Row, Select, Space, Typography } from 'antd'
 import FullWidthSpace from 'components/FullWidthSpace'
-import UploadImage from 'components/UploadImage'
-import UploadFileDocument from 'components/UploadFileDocument'
-import { allowFileExtensionsDocument, allowFileExtensionsImage } from 'config'
 import React, { useEffect, useState } from 'react'
 import { LeadCreateFormProps } from '../interface'
+import ReactDragListView from 'react-drag-listview'
+import { FormListFieldData } from 'antd/lib/form/FormList'
+import { move } from 'utils/utils'
 
-const { TextArea } = Input
 const { Option } = Select
 
 const { Title } = Typography
 
-
-
 const ruleRequired = {
-    required: true,
-    message: 'Required',
+  required: true,
+  message: 'Required',
 }
 
 const LeadCreateForm: React.FC<LeadCreateFormProps> = ({ product, form, loading, onFinish, onCancel }) => {
-    const [propertyType, setPropertyType] = useState('TEXT')
+  const [propertyType, setPropertyType] = useState('TEXT')
+  const [formData, setFormDate] = useState<any>([])
 
-    const handleFinished = (values: any) => {
-        onFinish?.({
-            ...values,
-            type: propertyType
-        })
-    }
+  const handleFinished = (values: any) => {
+    onFinish?.({
+      ...values,
+      type: propertyType,
+    })
+  }
 
+  const onDragEnd = (fromIndex: any, toIndex: any, index: number) => {
+    // console.log("Form Index:", fromIndex);
+    // console.log("To Index:", toIndex);
+    // console.log("Values", formData);
+    if (toIndex < 0) return
+    const newData = move(formData, fromIndex, toIndex)
+    form.setFieldsValue({
+      radios: newData,
+    })
+  }
 
-    const switchPorperty = (id: string) => {
-        switch (id) {
-            case 'TEXT':
-                return <><Input style={{ width: 221 }} /></>
-            case 'NUMBER':
-                return <><Input type={'number'} style={{ width: 221 }} /></>
-            case 'RADIO':
-                return <>
-                    <Input style={{ width: 221 }} />
-                    <DeleteOutlined style={{ color: "#FF0000", fontSize: 22, marginLeft: 10, marginRight: 10 }} />
-                    <Button type={'primary'} ghost icon={<PlusCircleOutlined />} >
-                        เพิ่มตัวเลือก
-                    </Button>
-                </>
-            case 'CHECKBOX':
-                return <>
-                    <Input style={{ width: 221 }} />
-                    <DeleteOutlined style={{ color: "#FF0000", fontSize: 22, marginLeft: 10, marginRight: 10 }} />
-                    <Button type={'primary'} ghost icon={<PlusCircleOutlined />} >
-                        เพิ่มตัวเลือก
-                    </Button>
-                </>
-            default:
-                return <><Input style={{ width: 221 }} /></>
-        }
-    }
-
-
-
-
-
-
-
-    return (
-        <Form
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 20 }}
-            id="content-pack"
-            name="content-pack"
-            form={form}
-            onFinish={handleFinished}
-            onValuesChange={() => { }}
-            labelAlign="left"
-        >
-            <Title level={5} style={{ color: '#2699FB', marginBottom: 30 }}>ข้อมูลคุณสมบัติ</Title>
-            <Form.Item name="name" label="ชื่อคุณสมบัติ" rules={[ruleRequired]}>
-                <Input placeholder="ชื่อคุณสมบัติ" style={{ width: 221 }} />
-            </Form.Item>
-            <Form.Item name="propertyType" label="Property Type">
-                <Select
-                    showArrow
-                    style={{ width: 207 }}
-                    placeholder="Please Select"
-                    filterOption={false}
-                    defaultValue={propertyType}
-                    onChange={(value) => {
-                        setPropertyType(value);
-                    }}
-                >
-                    <Option value="TEXT">ข้อความ</Option>
-                    <Option value="NUMBER">ตัวเลข</Option>
-                    <Option value="CHECKBOX">Checkbox</Option>
-                    <Option value="RADIO">Radio</Option>
-                </Select>
-            </Form.Item>
-            <Row>
-                <Col span={4} ></Col>
-                <Col span={20}>
-                    {switchPorperty(propertyType)}
-                </Col>
-            </Row>
-            <Form.Item>
-                <FullWidthSpace style={{ display: 'flex' }}>
+  const switchPorperty = (id: string) => {
+    switch (id) {
+      case 'TEXT':
+        return (
+          <>
+            <Input style={{ width: 221 }} />
+          </>
+        )
+      case 'NUMBER':
+        return (
+          <>
+            <Input type={'number'} style={{ width: 221 }} />
+          </>
+        )
+      case 'RADIO':
+        return (
+          <>
+            <Form.List name="radios">
+              {(fields, { add, remove }) => (
+                <>
+                  <ReactDragListView
+                    nodeSelector=".ant-list-item.draggable-item"
+                    lineClassName="dragLine"
+                    onDragEnd={(fromIndex, toIndex) => onDragEnd(fromIndex, toIndex, fromIndex)}
+                  >
+                    {fields.map((field, index) => (
+                      <List.Item key={index} style={{ paddingBottom: 0 }} className="draggable-item">
+                        <Space key={field.key} align="baseline">
+                          <Form.Item {...field} label="" name={[field.name, 'radio']} className="form-item">
+                            <Input style={{ width: 221 }} />
+                          </Form.Item>
+                          <DragOutlined className="icon" />
+                          <DeleteOutlined onClick={() => remove(field.name)} />
+                        </Space>
+                      </List.Item>
+                    ))}
+                  </ReactDragListView>
+                  <Form.Item>
                     <Button
-                        type="primary"
-                        htmlType="submit"
-                        loading={loading}
-                        style={{ minWidth: '10em' }}
-                        icon={<PlusCircleOutlined />}
+                      type="dashed"
+                      onClick={() => add()}
+                      icon={<PlusOutlined />}
+                      style={{
+                        borderColor: 'rgb(38 153 251)',
+                        color: 'rgb(38 153 251)',
+                        width: 150,
+                      }}
                     >
-                        Save
+                      เพิ่มตัวเลือก
                     </Button>
-                </FullWidthSpace>
-            </Form.Item>
-        </Form>
-    )
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+          </>
+        )
+      case 'CHECKBOX':
+        return (
+          <>
+            <Form.List name="checkboxs">
+              {(fields, { add, remove }) => (
+                <>
+                  <ReactDragListView
+                    nodeSelector=".ant-list-item.draggable-item"
+                    lineClassName="dragLine"
+                    onDragEnd={(fromIndex, toIndex) => onDragEnd(fromIndex, toIndex, fromIndex)}
+                  >
+                    {fields.map((field, index) => (
+                      <List.Item key={index} style={{ paddingBottom: 0 }} className="draggable-item">
+                        <Space key={field.key} align="baseline">
+                          <Form.Item {...field} label="" name={[field.name, 'checkbox']} className="form-item">
+                            <Input style={{ width: 221 }} />
+                          </Form.Item>
+                          <DragOutlined className="icon" />
+                          <DeleteOutlined onClick={() => remove(field.name)} />
+                        </Space>
+                      </List.Item>
+                    ))}
+                  </ReactDragListView>
+                  <Form.Item>
+                    <Button
+                      type="dashed"
+                      onClick={() => add()}
+                      icon={<PlusOutlined />}
+                      style={{
+                        borderColor: 'rgb(38 153 251)',
+                        color: 'rgb(38 153 251)',
+                        width: 150,
+                      }}
+                    >
+                      เพิ่มตัวเลือก
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+          </>
+        )
+      default:
+        return (
+          <>
+            <Input style={{ width: 221 }} />
+          </>
+        )
+    }
+  }
+
+  return (
+    <Form
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 18 }}
+      id="content-pack"
+      name="content-pack"
+      form={form}
+      onFinish={handleFinished}
+      onFieldsChange={(values) => {
+        if (propertyType === 'RADIO') {
+          setFormDate(form.getFieldValue('radios'))
+        }
+        if (propertyType === 'CHECKBOX') {
+          setFormDate(form.getFieldValue('checkboxs'))
+        }
+      }}
+      labelAlign="left"
+    >
+      <Title level={5} style={{ color: '#2699FB', marginBottom: 30 }}>
+        ข้อมูลคุณสมบัติ
+      </Title>
+      <Form.Item name="name" label="ชื่อคุณสมบัติ" rules={[ruleRequired]}>
+        <Input placeholder="ชื่อคุณสมบัติ" style={{ width: 221 }} />
+      </Form.Item>
+      <Form.Item name="propertyType" label="Property Type">
+        <Select
+          showArrow
+          style={{ width: 207 }}
+          placeholder="Please Select"
+          filterOption={false}
+          defaultValue={propertyType}
+          onChange={(value) => {
+            setPropertyType(value)
+          }}
+        >
+          <Option value="TEXT">ข้อความ</Option>
+          <Option value="NUMBER">ตัวเลข</Option>
+          <Option value="CHECKBOX">Checkbox</Option>
+          <Option value="RADIO">Radio</Option>
+        </Select>
+      </Form.Item>
+      <Row>
+        <Col span={6}></Col>
+        <Col span={18}>{switchPorperty(propertyType)}</Col>
+      </Row>
+      <Form.Item>
+        <FullWidthSpace style={{ display: 'flex' }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            style={{ minWidth: '10em' }}
+            icon={<PlusCircleOutlined />}
+          >
+            Save
+          </Button>
+        </FullWidthSpace>
+      </Form.Item>
+    </Form>
+  )
 }
 
 export default LeadCreateForm
--- utils/utils.ts
+++ utils/utils.ts
@@ -36,5 +36,22 @@ export const convertCSVToJSON = <T = unknown>(file: RcFile, config?: ParseConfig
   })
 
 export const dateUnixFormatter = (date: number) => {
-  return moment.unix(date/1000).format("DD - MM - YYYY");
+  return moment.unix(date / 1000).format('DD - MM - YYYY')
+}
+
+export function move(arr: [], old_index: number, new_index: number) {
+  while (old_index < 0) {
+    old_index += arr.length
+  }
+  while (new_index < 0) {
+    new_index += arr.length
+  }
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length
+    while (k-- + 1) {
+      arr.push()
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0])
+  return arr
 }
