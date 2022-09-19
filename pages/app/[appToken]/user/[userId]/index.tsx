882a9fc3f0e5d3b56323960249c38189ed1d8f69
kittipongkp <kittipong.na@littlebeansoft.com>
2022-09-19 18:57:33 +0700

    chnge menu

---
+++ pages/app/[appToken]/user/[userId]/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import MainLayout from 'layouts/MainLayout/MainLayout'
+import TitleComponent from 'components/TitleComponent'
+import withAuth from 'middlewares/withAuth'
+import { useRouter } from 'next/router'
+import UseDetailCard from 'components/User/UserDetail/UseDetailCard'
+
+const UserDetail: React.FC = () => {
+  const router = useRouter()
+
+  return (
+    <MainLayout breadcrumb={['Home', 'User Name']}>
+      <TitleComponent
+        title="User Name"
+        onBack={() =>
+          router.push({
+            pathname: `/org/[orgToken]/user`,
+            query: {
+              ...router.query,
+            },
+          })
+        }
+      />
+      <UseDetailCard />
+    </MainLayout>
+  )
+}
+
+export default withAuth(UserDetail)
