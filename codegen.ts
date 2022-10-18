72f39af9da36b599aaf067b4f001c50062bc5034
kritsada <newtronetic@gmail.com>
2022-10-18 17:19:01 +0700

    apply new codegen

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://kara-content-service.hooray.site/graphql',
+  documents: 'src/**/*.tsx',
+  generates: {
+    'src/gql': {
+      preset: 'client',
+      plugins: [],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+}
+
+export default config
