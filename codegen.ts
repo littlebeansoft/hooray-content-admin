7714e0e0aac6020f1ae449eebe02b9f6df846900
kritsada <newtronetic@gmail.com>
2022-10-19 10:31:40 +0700

    codegen

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,11 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://kara-content-service.hooray.site/graphql',
+  schema: [
+    'https://kara-core-service.hooray.site/graphql/admin/v1.0',
+    'https://kara-core-service.hooray.site/graphql/system-admin/v1.0',
+    'http://kara-content-service.hooray.site/graphql',
+  ],
   documents: 'src/**/*.tsx',
   generates: {
     'src/gql': {
