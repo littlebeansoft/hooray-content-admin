3e2d1253fec57bdebd91508d84f928ad5f6b70da
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-07 15:57:15 +0700

    fix: Changed path of links

--- src/setup/apollo/client/core.ts
+++ src/setup/apollo/client/core.ts
@@ -5,7 +5,7 @@ import {
   InMemoryCache,
 } from '@apollo/client'
 
-import { authLink, errorLink } from 'setup/apollo'
+import { authLink, errorLink } from 'setup/apollo/link'
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_CORE_ENDPOINT,
