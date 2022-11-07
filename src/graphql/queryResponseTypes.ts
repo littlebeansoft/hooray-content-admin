37c9a82f009bfec269684c974adad524c5a4c193
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-07 14:00:21 +0700

    feat: Extract types from graphql codegen

---
+++ src/graphql/queryResponseTypes.ts
@@ -0,0 +1,7 @@
+import { GetAuthUserQuery } from './__generated/operations'
+
+export type QR_GetOrgAccessTokenWithRefResult =
+  GetAuthUserQuery['getOrgAccessTokenWithRef']['payload']
+
+export type QR_GetAppByCredentialResult =
+  GetAuthUserQuery['getAppByCredential']['payload']
