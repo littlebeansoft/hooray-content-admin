55b47fac8899a0c59b345ef820774dabcca82834
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-09 12:25:18 +0700

    feat: Added route to helper

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,7 +1,27 @@
+import { generatePath } from 'react-router-dom'
+
+import { stringify, ParsedQuery } from 'query-string'
+
+interface RouteToOptions {
+  params?: Record<string, string>
+  query?: ParsedQuery
+}
+
 export const getSearchParams = () => {
   return new URLSearchParams(window.location.search)
 }
 
 export const getCredentialKeyFromQueryString = () => {
   return getSearchParams().get('credentialKey')
+}
+
+export const routeTo = (path: string, options?: RouteToOptions) => {
+  const pathname = generatePath(path, options?.params)
+
+  const query = {
+    ...options?.query,
+    credentialKey: getCredentialKeyFromQueryString(),
+  }
+
+  return `${pathname}?${stringify(query)}`
 }
