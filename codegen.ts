c6f8db3ef29dedd463b9f8e2987341f4e9d895de
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-09 10:47:38 +0700

    feat: Added relevant schema URLs

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,11 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: ['https://kara-core-service.hooray.site/graphql/admin/v1.0'],
+  schema: [
+    'https://kara-core-service.hooray.site/graphql/admin/v1.0',
+    'https://kara-content-service.hooray.site/graphql',
+    'https://kara-label-service.hooray.site/graphql',
+  ],
   ignoreNoDocuments: true,
   documents: 'src/graphql/documents/**/*.graphql',
   generates: {
