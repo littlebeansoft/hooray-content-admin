28021344d97eedef743148caffe41be7c6817052
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-09 16:14:58 +0700

    fix: Used variable from routes config instead

--- src/setup/PageRouter/PageRouter.tsx
+++ src/setup/PageRouter/PageRouter.tsx
@@ -3,11 +3,10 @@ import { QueryParamProvider } from 'use-query-params'
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6'
 
 import ErrorPage from 'pages/ErrorPage'
-import ContentPage from 'pages/content/IndexPage'
 
 import { appAuthValidUser, orgAuthValidUser } from 'helpers/auth'
 
-import { paths } from 'setup/PageRouter/routes'
+import { appRouteConfig, orgRouteConfig } from 'setup/PageRouter/routes'
 
 const PageRouter = () => {
   const router = createBrowserRouter([
@@ -18,38 +17,12 @@ const PageRouter = () => {
         {
           loader: orgAuthValidUser,
           errorElement: <ErrorPage />,
-          children: [
-            {
-              path: paths.orgContent,
-              element: <ContentPage />,
-            },
-            {
-              path: paths.orgContentCreate,
-              element: <h1>This is org content create page</h1>,
-            },
-            {
-              path: paths.orgContentUpdate,
-              element: <h1>This is org content update page</h1>,
-            },
-          ],
+          children: orgRouteConfig,
         },
         {
           loader: appAuthValidUser,
           errorElement: <ErrorPage />,
-          children: [
-            {
-              path: paths.appContent,
-              element: <h1>This is app content page</h1>,
-            },
-            {
-              path: paths.appContentCreate,
-              element: <h1>This is app content create page</h1>,
-            },
-            {
-              path: paths.appContentUpdate,
-              element: <h1>This is app content update page</h1>,
-            },
-          ],
+          children: appRouteConfig,
         },
       ],
     },
