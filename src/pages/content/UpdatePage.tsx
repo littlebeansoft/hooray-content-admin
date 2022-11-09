38a39e882bdfa15646c115b6f7638830ef914545
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-09 16:14:46 +0700

    feat: Added content update page

---
+++ src/pages/content/UpdatePage.tsx
@@ -0,0 +1,11 @@
+import PageTitle from 'components/PageTitle'
+
+const ContentUpdatePage = () => {
+  return (
+    <>
+      <PageTitle>แก้ไขหลักสูตร</PageTitle>
+    </>
+  )
+}
+
+export default ContentUpdatePage
