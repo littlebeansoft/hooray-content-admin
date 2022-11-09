04336f73c29d2b953ee2a9e2a7544b97cf0d0bc6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-09 16:14:38 +0700

    feat: Added content create page

---
+++ src/pages/content/CreatePage.tsx
@@ -0,0 +1,11 @@
+import PageTitle from 'components/PageTitle'
+
+const ContentCreatePage = () => {
+  return (
+    <>
+      <PageTitle>สร้างหลักสูตร</PageTitle>
+    </>
+  )
+}
+
+export default ContentCreatePage
