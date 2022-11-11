70e247105472ce8c8cff7c33cea1c0a60fd7c487
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-11 14:11:19 +0700

    feat: Added utils for upload file

---
+++ src/components/UploadFile/utils.ts
@@ -0,0 +1,50 @@
+import { RcFile } from 'antd/lib/upload'
+
+type FileType = 'video' | 'image' | 'document'
+
+const isImageFile = (file: string) =>
+  /\.(gif|jpg|jpeg|tiff?|png|webp|bmp)$/i.test(file)
+
+const isVideoFile = (file: string) => /\.(mp4|mp5)$/i.test(file)
+
+const isBase64 = (file: string) => file.includes('base64')
+
+const getFileType = (file: string): FileType => {
+  if (isImageFile(file)) {
+    return 'image'
+  }
+
+  if (isVideoFile(file)) {
+    return 'video'
+  }
+
+  return 'document'
+}
+
+const getFileTypeFromBase64 = (file: string) => {
+  const extension = file.split(';')[0].split('/')[1]
+
+  return getFileType(`.${extension}`)
+}
+
+export const getBase64 = (file: RcFile): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.readAsDataURL(file)
+
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = (error) => reject(error)
+  })
+
+export const getFileExtensionType = (file?: string): FileType => {
+  if (file == null) {
+    return 'image'
+  }
+
+  if (isBase64(file)) {
+    return getFileTypeFromBase64(file)
+  }
+
+  return getFileType(file)
+}
