4195f62cf69d66ea1e05a5951e88d641732506e5
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-11 12:58:21 +0700

    feat: Added hook for uploading file to s3

---
+++ src/hooks/useUploadFileToS3.ts
@@ -0,0 +1,49 @@
+import type { RcFile } from 'antd/lib/upload'
+
+import { useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+
+const useUploadFile = <T = any>() => {
+  const [uploading, setUploading] = useState(false)
+  const [uploadPercent, setUploadPercent] = useState(0)
+
+  const uploadFileToS3 = async (
+    url: string,
+    file: RcFile
+  ): Promise<AxiosResponse<T>> => {
+    setUploading(true)
+
+    return new Promise((resolve, reject) =>
+      axios
+        .request<T>({
+          method: 'PUT',
+          url,
+          data: file,
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total = 0 } = progressEvent
+            const uploadPercent = Math.ceil((100 / total) * loaded)
+
+            setUploadPercent(uploadPercent)
+          },
+        })
+        .then(async (resp) => {
+          resolve(resp)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+        .finally(async () => {
+          setUploading(false)
+          setUploadPercent(0)
+        })
+    )
+  }
+
+  return {
+    uploading,
+    uploadPercent,
+    uploadFileToS3,
+  }
+}
+
+export default useUploadFile
