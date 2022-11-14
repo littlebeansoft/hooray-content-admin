3e154c76eba8329c36e2f08607d41d3c9a6741f8
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-14 16:57:25 +0700

    feat: Added rule required utils

--- src/helpers/antdUtils.ts
+++ src/helpers/antdUtils.ts
@@ -1,6 +1,13 @@
+import type { Rule } from 'antd/lib/form'
+
 import { FormProps } from 'antd'
 
 export const baseFormProps: FormProps = {
   layout: 'vertical',
   colon: false,
+}
+
+export const ruleRequired: Rule = {
+  required: true,
+  message: 'โปรดระบุ',
 }
