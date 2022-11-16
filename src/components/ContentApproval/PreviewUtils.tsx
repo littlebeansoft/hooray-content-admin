1283266d523595c2f6a06e2239f240f1889de663
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-17 03:42:31 +0700

    feat: Added preview utils component

---
+++ src/components/ContentApproval/PreviewUtils.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+export const DisplayLabel = styled.p`
+  color: #8c8c8c;
+  margin-bottom: 16px;
+`
+
+export const DisplayValue = styled.span`
+  color: #444;
+`
+
+export const NotFoundContent = styled.span`
+  color: #ddd;
+`
