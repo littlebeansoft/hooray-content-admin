7bbed4549195e357ff25ce8087d3475644ac2d2a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-16 16:15:27 +0700

    feat: Added API connection

--- src/pages/content/CreatePage.tsx
+++ src/pages/content/CreatePage.tsx
@@ -1,14 +1,48 @@
+import { useNavigate } from 'react-router-dom'
+import { message, Space } from 'antd'
+
 import ContentForm, { useParentFormInstance } from 'components/Content/Form'
 import PageTitle from 'components/PageTitle'
+
+import { useCreateContentPackMutation } from 'graphql/__generated/operations'
+import { LeftOutlined } from '@ant-design/icons'
 
 const ContentCreatePage = () => {
   const [form] = useParentFormInstance()
 
+  const navigate = useNavigate()
+
+  const [create, { loading }] = useCreateContentPackMutation({
+    onCompleted() {
+      navigate(-1)
+    },
+    onError(error) {
+      console.log(error)
+
+      message.error('ไม่สามารถสร้างหลักสูตรได้ โปรดลองใหม่อีกครั้ง')
+    },
+  })
+
   return (
     <>
-      <PageTitle>สร้างหลักสูตร</PageTitle>
+      <PageTitle>
+        <Space>
+          <LeftOutlined onClick={() => navigate(-1)} />
+          สร้างหลักสูตร
+        </Space>
+      </PageTitle>
 
-      <ContentForm form={form} />
+      <ContentForm
+        loading={loading}
+        form={form}
+        onFinish={(values) =>
+          create({
+            variables: {
+              input: values,
+            },
+          })
+        }
+      />
     </>
   )
 }
