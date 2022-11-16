b756497940c7ed88a2c86be9485527da2d81388b
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-16 14:41:23 +0700

    feat: Added content create page

--- src/pages/content/CreatePage.tsx
+++ src/pages/content/CreatePage.tsx
@@ -1,9 +1,14 @@
+import ContentForm, { useParentFormInstance } from 'components/Content/Form'
 import PageTitle from 'components/PageTitle'
 
 const ContentCreatePage = () => {
+  const [form] = useParentFormInstance()
+
   return (
     <>
       <PageTitle>สร้างหลักสูตร</PageTitle>
+
+      <ContentForm form={form} />
     </>
   )
 }
