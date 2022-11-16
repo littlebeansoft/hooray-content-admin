41c4485ca7139799b519c9af907788f5756896c1
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-16 16:13:59 +0700

    feat: Added form creator section

---
+++ src/components/Content/FormCreator.tsx
@@ -0,0 +1,37 @@
+import { Col, Form, Row, Select } from 'antd'
+
+import ContentBlock from 'components/ContentBlock'
+
+import { useGetListCreatorsProfileQuery } from 'graphql/__generated/operations'
+
+import { coreClient } from 'setup/apollo'
+
+const FormCreator = () => {
+  const query = useGetListCreatorsProfileQuery({
+    client: coreClient,
+  })
+
+  const listCreators = query.data?.getResourceAdmin.payload
+
+  return (
+    <ContentBlock title="ข้อมูลผู้สอน">
+      <Row gutter={[32, 16]}>
+        <Col span={24}>
+          <Form.Item label="ผู้สอน">
+            <Select mode="multiple" showSearch>
+              {listCreators?.map((item) => (
+                <Select.Option key={item._id} value={item._id}>
+                  <span>
+                    {item.attribute.firstName} {item.attribute.lastName}
+                  </span>
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+    </ContentBlock>
+  )
+}
+
+export default FormCreator
