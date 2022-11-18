09cde4a39363d6f7c95d774a922ddd02c6ba43f6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-18 17:28:16 +0700

    feat: Added category create page

---
+++ src/pages/category/CreatePage.tsx
@@ -0,0 +1,13 @@
+import PageTitle from 'components/PageTitle'
+
+const CategoryCreatePage = () => {
+  return (
+    <>
+      <PageTitle>สร้างหมวดหมู่หลักสูตร</PageTitle>
+
+      <h1>สร้างหมวดหมู่</h1>
+    </>
+  )
+}
+
+export default CategoryCreatePage
