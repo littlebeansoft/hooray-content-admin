416310503034720377a47675dce85682ce35863a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-18 18:16:56 +0700

    feat: Added category list

--- src/pages/category/IndexPage.tsx
+++ src/pages/category/IndexPage.tsx
@@ -1,11 +1,24 @@
+import CategoryList, { useGetListCategories } from 'components/Category/List'
 import PageTitle from 'components/PageTitle'
 
 const CategoryPage = () => {
+  const { query, filter, pagination, onFilterChange, onTableChange } =
+    useGetListCategories()
+
+  const dataSource = query.data?.getCategory.payload
+
   return (
     <>
       <PageTitle>หมวดหมู่หลักสูตร</PageTitle>
 
-      <h1>หมวดหมู่</h1>
+      <CategoryList
+        loading={query.loading}
+        dataSource={dataSource}
+        filter={filter}
+        pagination={pagination}
+        onFilterChange={onFilterChange}
+        onTableChange={onTableChange}
+      />
     </>
   )
 }
