91657eb51c790dcf886cd21c5efb1f6ec539a4a3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-21 11:07:34 +0700

    feat: Used th locale for antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ConfigProvider } from 'antd'
 import { ApolloProvider } from '@apollo/client'
+import thTH from 'antd/lib/locale/th_TH'
 
 import PageRouter from 'setup/PageRouter'
 import { contentClient } from 'setup/apollo'
@@ -7,6 +8,7 @@ import { contentClient } from 'setup/apollo'
 const App = () => {
   return (
     <ConfigProvider
+      locale={thTH}
       componentSize="large"
       space={{
         size: 'large',
