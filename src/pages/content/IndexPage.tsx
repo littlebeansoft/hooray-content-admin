677b1d775c866be43da5a782e654ee8a8cfeea62
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-22 23:08:30 +0700

    feat: Added content list

--- src/pages/content/IndexPage.tsx
+++ src/pages/content/IndexPage.tsx
@@ -1,9 +1,33 @@
+import { LeftOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
+
+import ContentList, { useGetContentList } from 'components/Content/List'
 import PageTitle from 'components/PageTitle'
 
 const ContentPage = () => {
+  const { query, filter, pagination, onFilterChange, onTableChange } =
+    useGetContentList()
+
+  const navigate = useNavigate()
+
+  const section = query.data?.getSectionByID.payload
+  const dataSource = query.data?.getContentList.payload
+
   return (
     <>
-      <PageTitle>เนื้อหา</PageTitle>
+      <PageTitle>
+        <LeftOutlined onClick={() => navigate(-1)} />
+        เนื้อหาของบทเรียน {section?.title}
+      </PageTitle>
+
+      <ContentList
+        loading={query.loading}
+        filter={filter}
+        dataSource={dataSource}
+        pagination={pagination}
+        onFilterChange={onFilterChange}
+        onTableChange={onTableChange}
+      />
     </>
   )
 }
