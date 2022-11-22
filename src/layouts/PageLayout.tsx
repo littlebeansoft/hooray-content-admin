35e799a63de0d3020e81947765e8c1d96dfcb3a4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-22 12:45:09 +0700

    feat: Added version component

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 
 import { Layout } from 'antd'
+import styled from '@emotion/styled'
 
 interface PageLayoutProps {
   children: ReactNode
@@ -11,9 +12,24 @@ const { Content } = Layout
 const PageLayout = ({ children }: PageLayoutProps) => {
   return (
     <Layout>
-      <Content id="main-content">{children}</Content>
+      <Content id="main-content">
+        {children}
+
+        <Version>v{process.env.REACT_APP_VERSION}</Version>
+      </Content>
     </Layout>
   )
 }
 
 export default PageLayout
+
+const Version = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  font-weight: bold;
+  font-size: 12px;
+  padding: 2px 4px;
+  background: #f1f1f1;
+  border-radius: 6px;
+`
