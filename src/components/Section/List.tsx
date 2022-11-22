6c2d68125debb3c3a5a612fb7c1e9d565fe46f3b
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-22 18:59:20 +0700

    feat: Added search by keyword from query string

--- src/components/Section/List.tsx
+++ src/components/Section/List.tsx
@@ -69,6 +69,9 @@ export const useGetSectionList = () => {
         filter: {
           active: getActiveBooleanValue(filter.active),
         },
+        search: {
+          title: filter.keyword == null ? undefined : filter.keyword,
+        },
       },
     },
   })
