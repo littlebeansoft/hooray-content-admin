3fdee1474fc03ef76ace7e3f728c9b19c7c787b6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-22 18:59:04 +0700

    feat: Added create section modal

--- src/components/Section/ListControl.tsx
+++ src/components/Section/ListControl.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import { PlusOutlined } from '@ant-design/icons'
-import { Button, Col, Input, Row, Space } from 'antd'
+import { useParams } from 'react-router-dom'
+import { Button, Col, Input, message, Row, Space } from 'antd'
 
 import ActiveSelectInput from 'components/ActiveSelectInput'
+import SectionFormModal, { useParentFormInstance } from './FormModal'
+
+import { useCreateSectionMutation } from 'graphql/__generated/operations'
 
 export type FilterKey = 'active' | 'keyword'
 
@@ -18,27 +23,89 @@ const SectionListControl = ({
   filter,
   onFilterChange,
 }: SectionListControlProps) => {
+  const [form] = useParentFormInstance()
+
+  const params = useParams()
+  const contentPackID = params.id as string
+
+  const [openCreateSectionModal, setOpenCreateSectionModal] = useState(false)
+
+  const [create, { loading }] = useCreateSectionMutation({
+    refetchQueries: ['getSectionList'],
+    onCompleted() {
+      setOpenCreateSectionModal(false)
+
+      message.success('สร้างบทเรียนสำเร็จ')
+    },
+    onError(error) {
+      console.log(error)
+
+      message.error('ไม่สามารถสร้างบทเรียนได้ โปรดลองใหม่อีกครั้ง')
+    },
+  })
+
   return (
-    <Row gutter={[32, 32]} align="bottom">
-      <Col span={16}>
-        <Space align="end">
-          <ActiveSelectInput filter={filter} onFilterChange={onFilterChange} />
-          <Input.Search
-            allowClear
-            placeholder="ค้นหาบทเรียน"
-            defaultValue={filter?.keyword || undefined}
-            onSearch={(value) => onFilterChange?.('keyword', value)}
-          />
-        </Space>
-      </Col>
-      <Col span={8}>
-        <div style={{ textAlign: 'right' }}>
-          <Button icon={<PlusOutlined />} type="primary">
-            สร้างบทเรียน
-          </Button>
-        </div>
-      </Col>
-    </Row>
+    <>
+      <Row gutter={[32, 32]} align="bottom">
+        <Col span={16}>
+          <Space align="end">
+            <ActiveSelectInput
+              filter={filter}
+              onFilterChange={onFilterChange}
+            />
+            <Input.Search
+              allowClear
+              placeholder="ค้นหาบทเรียน"
+              defaultValue={filter?.keyword || undefined}
+              onSearch={(value) => onFilterChange?.('keyword', value)}
+            />
+          </Space>
+        </Col>
+        <Col span={8}>
+          <div style={{ textAlign: 'right' }}>
+            <Button
+              icon={<PlusOutlined />}
+              type="primary"
+              onClick={() => setOpenCreateSectionModal(true)}
+            >
+              สร้างบทเรียน
+            </Button>
+          </div>
+        </Col>
+      </Row>
+
+      <SectionFormModal
+        form={form}
+        onFinish={(values) => {
+          create({
+            variables: {
+              input: {
+                ...values,
+                contentPackID,
+              },
+            },
+          })
+        }}
+        modalProps={{
+          title: 'สร้างบทเรียน',
+          open: openCreateSectionModal,
+          onCancel: () => setOpenCreateSectionModal(false),
+          footer: (
+            <div style={{ textAlign: 'right' }}>
+              <Space>
+                <Button onClick={() => setOpenCreateSectionModal(false)}>
+                  ยกเลิก
+                </Button>
+
+                <Button loading={loading} type="primary" onClick={form.submit}>
+                  สร้างบทเรียน
+                </Button>
+              </Space>
+            </div>
+          ),
+        }}
+      />
+    </>
   )
 }
 
