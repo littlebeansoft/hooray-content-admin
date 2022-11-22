df4df3e3a10084aa68c64bf4e75816fb3f084411
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-22 17:01:26 +0700

    fix: Changed number format without decimal

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -6,7 +6,7 @@ export const TIME_FORMAT = 'HH:mm'
 export const DATE_TIME_FORMAT = `${DATE_FORMAT} ${TIME_FORMAT}`
 export const HUMAN_READABLE_FORMAT = `DD MMM YYYY - ${TIME_FORMAT}`
 
-export const numberFormat = (value?: string | number, format = '0,0.00') => {
+export const numberFormat = (value?: string | number, format = '0,0') => {
   return numeral(value).format(format)
 }
 
