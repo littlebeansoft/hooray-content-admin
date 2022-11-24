fa8241faf01e4eec01ab6b7f858e84e22e63eef3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-24 10:01:41 +0700

    fix: Used new design for creator selection

--- src/components/ContentPack/FormCreator.tsx
+++ src/components/ContentPack/FormCreator.tsx
@@ -1,32 +1,15 @@
-import { Col, Form, Row, Select } from 'antd'
+import { Col, Form, Row } from 'antd'
 
 import ContentBlock from 'components/ContentBlock'
-
-import { useGetListCreatorsProfileQuery } from 'graphql/__generated/operations'
-
-import { coreClient } from 'setup/apollo'
+import CreatorSelectTable from 'components/CreatorSelectTable'
 
 const ContentPackFormCreator = () => {
-  const query = useGetListCreatorsProfileQuery({
-    client: coreClient,
-  })
-
-  const listCreators = query.data?.getResourceAdmin.payload
-
   return (
     <ContentBlock title="ข้อมูลผู้สอน">
       <Row gutter={[32, 16]}>
         <Col span={24}>
-          <Form.Item label="ผู้สอน" name="creatorIDs">
-            <Select mode="multiple" showSearch>
-              {listCreators?.map((item) => (
-                <Select.Option key={item._id} value={item._id}>
-                  <span>
-                    {item.attribute.firstName} {item.attribute.lastName}
-                  </span>
-                </Select.Option>
-              ))}
-            </Select>
+          <Form.Item name="creatorIDs" noStyle>
+            <CreatorSelectTable />
           </Form.Item>
         </Col>
       </Row>
