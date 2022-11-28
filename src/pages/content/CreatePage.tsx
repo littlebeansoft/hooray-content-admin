e6f4ce7df87ca14855cdc6b1db6ed74df9cb6346
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-11-28 19:18:55 +0700

    feat: Connected to API for create content

--- src/pages/content/CreatePage.tsx
+++ src/pages/content/CreatePage.tsx
@@ -1,13 +1,32 @@
-import { useNavigate } from 'react-router-dom'
+import { message } from 'antd'
+import { useNavigate, useParams } from 'react-router-dom'
 import { LeftOutlined } from '@ant-design/icons'
 
 import PageTitle from 'components/PageTitle'
 import ContentForm, { useParentFormInstance } from 'components/Content/Form'
 
+import { useCreateContentMutation } from 'graphql/__generated/operations'
+
 const ContentCreatePage = () => {
   const navigate = useNavigate()
 
+  const params = useParams()
+  const sectionID = params.sectionID as string
+
   const [form] = useParentFormInstance()
+
+  const [create, { loading }] = useCreateContentMutation({
+    onCompleted() {
+      message.success('สร้างเนื้อหาสำเร็จ')
+
+      navigate(-1)
+    },
+    onError(error) {
+      console.log(error)
+
+      message.error('ไม่สามารถสร้างเนื้อหาได้')
+    },
+  })
 
   return (
     <>
@@ -16,7 +35,20 @@ const ContentCreatePage = () => {
         สร้างเนื้อหา
       </PageTitle>
 
-      <ContentForm form={form} />
+      <ContentForm
+        loading={loading}
+        form={form}
+        onFinish={(values) =>
+          create({
+            variables: {
+              input: {
+                ...values,
+                sectionID,
+              },
+            },
+          })
+        }
+      />
     </>
   )
 }
