e062f29b24a2c44bfdc3ac772163fbb60dd0e821
Seksun Sawisai <seksun.sa@littlebeansoft.com>
2022-11-28 20:57:32 +0700

    update jenkin and docker file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ENV_LIST=$(env | grep 'PUBLIC')
-for ENV in $ENV_LIST; do
-    echo "Setting environment: ${ENV}"
-    ENV_KEY=$(echo "$ENV" | sed 's;=.*;;')
-    ENV_VALUE=$(echo "$ENV" | sed 's;.*=;;')
-    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#CUSTOM_${ENV_KEY}#${ENV_VALUE}#g"
+APP_ENV_LIST=$(env | grep 'REACT_APP')
+for APP_ENV in $APP_ENV_LIST; do
+    echo "Setting environment: ${APP_ENV}"
+    APP_ENV_KEY=$(echo "$APP_ENV" | sed 's;=.*;;')
+    APP_ENV_VALUE=$(echo "$APP_ENV" | sed 's;.*=;;')
+    find /usr/share/nginx/html \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#CUSTOM_${APP_ENV_KEY}#${APP_ENV_VALUE}#g"
 done
 exec "$@"
